95cdf581eae26c1f2a0f2d1424a104c8639895d0
Ahmad Yusuf <ahmadee.by@gmail.com>
2023-10-18 17:06:17 +0100

    Final changes

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -1,27 +1,41 @@
-import Footer from '@/components/Footer'
-import NavBar from '@/components/NavBar'
-import Image from 'next/image'
-import React from 'react'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+import React from "react";
 
 export default function Members() {
   return (
-    <div className='bg-gray-50 w-full min-h-screen'>
+    <div className="bg-gray-50 w-full min-h-screen">
       <NavBar />
-      
+
       <div className="px-4 sm:px-8 md:px-12 lg:px-24 py-8 sm:py-12 md:py-16 lg:py-24 flex flex-col gap-y-6 sm:gap-y-8 items-start">
         <div className="flex flex-col sm:flex-row justify-start gap-4 sm:gap-6">
           <div className="relative w-52 md:w-80 h-48 md:h-56 bg-gray-300 rounded">
-            <Image src="/images/tajudeen-abbas.webp" layout='fill' className="" alt={''} />
+            <Image
+              src="/images/tajudeen-abbas.webp"
+              layout="fill"
+              className=""
+              alt={""}
+            />
           </div>
           <div className="w-full">
             <div className="flex flex-col gap-1">
-              <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">Hon. Abbas Tajudeen</h1>
-              <p className="text-gray-500 capitalize">Speaker of the House of Representatives</p>
+              <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">
+                Hon. Abbas Tajudeen
+              </h1>
+              <p className="text-gray-500 capitalize">
+                Speaker of the House of Representatives
+              </p>
             </div>
             <div className="flex flex-col w-full gap-4">
               <div className="pt-2 sm:pt-4">
                 <p className="text-gray-800 text-sm sm:text-base w-full sm:w-6/12">
-                  Abbas was born on 1 October 1963 in Kwarbai, Zaria, Kaduna State, Nigeria. He holds a bachelor's degree in business administration and a master's degree both from Ahmadu Bello University, Zaria. He later obtained his doctorate degree in business administration from Usmanu Danfodiyo University, Sokoto.
+                  Abbas was born on 1 October 1963 in Kwarbai, Zaria, Kaduna
+                  State, Nigeria. He holds a bachelor&apos;s degree in business
+                  administration and a master&apos;s degree both from Ahmadu Bello
+                  University, Zaria. He later obtained his doctorate degree in
+                  business administration from Usmanu Danfodiyo University,
+                  Sokoto.
                 </p>
               </div>
               <div className="flex flex-col sm:flex-row justify-between w-full sm:w-8/12">
@@ -34,7 +48,9 @@ export default function Members() {
                 <div className="">
                   <h6 className="text-gray-400 font-semibold">Address</h6>
                   <div className="flex flex-col gap-1 text-gray-800 text-sm">
-                    <p className="capitalize">no. 9b kaduna road by kofar kibo zaria, kaduna state</p>
+                    <p className="capitalize">
+                      no. 9b kaduna road by kofar kibo zaria, kaduna state
+                    </p>
                   </div>
                 </div>
               </div>
@@ -45,17 +61,29 @@ export default function Members() {
       <div className="px-4 sm:px-8 md:px-12 lg:px-24 py-4 sm:py-6 flex flex-col gap-y-6 sm:gap-y-8 items-start">
         <div className="flex flex-col sm:flex-row justify-start gap-4 sm:gap-6">
           <div className="relative w-48 md:w-72 h-48 md:h-56 bg-gray-300 rounded">
-            <Image src="/images/ahmed-munir.png" layout='fill' className="" alt={''} />
+            <Image
+              src="/images/ahmed-munir.png"
+              layout="fill"
+              className=""
+              alt={""}
+            />
           </div>
           <div className="w-full">
             <div className="flex flex-col gap-1">
-              <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">Hon. Ahmed Munir</h1>
-              <p className="text-gray-500 capitalize">Chairman, House Committee On Commerce</p>
+              <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">
+                Hon. Ahmed Munir
+              </h1>
+              <p className="text-gray-500 capitalize">
+                Chairman, House Committee On Commerce
+              </p>
             </div>
             <div className="flex flex-col w-full gap-4">
               <div className="pt-2 sm:pt-4">
                 <p className="text-gray-800 text-sm sm:text-base w-full sm:w-6/12">
-                  Hon. Ahmed Munir was born on the 9th day of September, 1980, in Saminaka, Lere Federal Constituency, Kaduna State, Nigeria. He is married to Farida Munir, who is a civil servant and a proud father of twin boys; Mohammed and Kameel Munir.
+                  Hon. Ahmed Munir was born on the 9th day of September, 1980,
+                  in Saminaka, Lere Federal Constituency, Kaduna State, Nigeria.
+                  He is married to Farida Munir, who is a civil servant and a
+                  proud father of twin boys; Mohammed and Kameel Munir.
                 </p>
               </div>
               <div className="flex flex-col sm:flex-row justify-between w-full sm:w-8/12">
@@ -68,7 +96,9 @@ export default function Members() {
                 <div className="">
                   <h6 className="text-gray-400 font-semibold">Address</h6>
                   <div className="flex flex-col gap-1 text-gray-800 text-sm">
-                    <p className="capitalize">ahmadu bello way, kaduna-jos road, saminaka, kaduna</p>
+                    <p className="capitalize">
+                      ahmadu bello way, kaduna-jos road, saminaka, kaduna
+                    </p>
                   </div>
                 </div>
               </div>
@@ -78,5 +108,5 @@ export default function Members() {
       </div>
       <Footer />
     </div>
-  )
+  );
 }
