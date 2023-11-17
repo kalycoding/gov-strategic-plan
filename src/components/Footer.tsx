72f10eb0a5246bf781ec9777b96457c7aa9be372
najiblere <kabir.najib@wayapaychat.com>
2023-11-17 19:56:49 +0100

    fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             </div>
         </div>
         <div className="">
-            <h5 className="text-sm text-secondary font-[700] uppercase">Download</h5>
+            <h5 className="text-sm text-secondary font-[700] uppercase">Downloads</h5>
             <div className="flex flex-col text-[#434546] text-xs pt-3 gap-2">
                 {/* <Link href={"tel:+2347065633727"}>Main: +234 706 5633 727</Link>
                 <Link href={"tel:+2347065633727"}>Press: +234 706 5633 727</Link> */}
